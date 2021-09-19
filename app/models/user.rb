dd30fb5fb64c942d2bb97599898b06afc22b94d1
Nicholas <berubenic@gmail.com>
2021-09-19 09:47:18 -0400

    add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  enum role: {
+    'WellChecker' => 0,
+    'WellChecker Recipient' => 1,
+    'Organization' => 2
+  }
 end
